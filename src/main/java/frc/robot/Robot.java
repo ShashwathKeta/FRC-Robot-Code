2273448fba2054296c1ba8584731459fabca8178
ShashwathKeta <shashwathketa@gmail.com>
2020-04-15 19:17:34 -0500

    Edited code to work with the D pad on controller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,7 +32,7 @@ public class Robot extends TimedRobot {
    * for any initialization code.
    */
 
-   //Make this a singleton class
+  // Make this a singleton class
   private final RobotConfig config = new RobotConfig();
   private final PWMSparkMax m_leftMotor1 = new PWMSparkMax(config.getLeftMotor1());
   private final PWMSparkMax m_leftMotor2 = new PWMSparkMax(config.getLeftMotor2());
@@ -56,9 +56,10 @@ public class Robot extends TimedRobot {
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
   private final SpeedControllerGroup m_ShooterMotor = new SpeedControllerGroup(m_ShooterMotor1, m_ShooterMotor2);
   private final SpeedControllerGroup m_leftMotors = new SpeedControllerGroup(m_leftMotor1, m_leftMotor2, m_leftMotor3);
-  private final SpeedControllerGroup m_rightMotors = new SpeedControllerGroup(m_rightMotor1, m_rightMotor2, m_rightMotor3);
+  private final SpeedControllerGroup m_rightMotors = new SpeedControllerGroup(m_rightMotor1, m_rightMotor2,
+      m_rightMotor3);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-  private final XboxController m_driverController = new XboxController(0);
+  private final XboxController m_driverController = new XboxController(1);
   private final Shooter m_Shooter = new Shooter();
   private final Climb m_Climb = new Climb();
   private int shooterConfig = -1;
@@ -66,33 +67,27 @@ public class Robot extends TimedRobot {
   private boolean isPositioning = false;
 
   @Override
-  public void robotInit() 
-  {  
+  public void robotInit() {
   }
 
   @Override
-  public void robotPeriodic() 
-  {
-    if(isPositioning)
-    {
+  public void robotPeriodic() {
+    if (isPositioning) {
       m_Climb.checkControlPanelPositioning(m_funnel);
     }
     m_Shooter.transferController(m_transferMotor, m_funnel, beamBreakBack, beamBreakFront);
   }
 
   @Override
-  public void autonomousInit() 
-  {
+  public void autonomousInit() {
   }
 
   @Override
-  public void autonomousPeriodic() 
-  {
+  public void autonomousPeriodic() {
   }
 
   @Override
-  public void teleopInit() 
-  {
+  public void teleopInit() {
     m_intakeRotationMotor.setPosition(0);
     m_intakeMotor.setSpeed(0);
     m_Shooter.shooterConfig(-1, m_ShooterMotor, m_hoodMotor);
@@ -100,153 +95,137 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() 
+  public void teleopPeriodic()
   {
     m_Shooter.periodicSpeed(shooterConfig, m_ShooterMotor, m_hoodMotor);
     m_robotDrive.arcadeDrive(m_driverController.getY(Hand.kLeft), m_driverController.getX(Hand.kRight));
     m_Shooter.autoalign(m_turretMotor);
+    m_Climb.stopBarRunner(m_funnel);
 
-    if(m_driverController.getXButtonPressed())
-    {
+    if (m_driverController.getXButtonPressed()) {
       m_intakeRotationMotor.setPosition(.25);
       m_intakeMotor.setSpeed(.8);
       m_funnel.setSpeed(.8);
+      System.out.print("X Button Clicked");
     }
 
-    else if(m_driverController.getXButtonReleased())
-    {
+    else if (m_driverController.getXButtonReleased()) {
       m_intakeRotationMotor.setPosition(0);
       m_intakeMotor.setSpeed(0);
     }
 
-    if(m_driverController.getAButtonPressed())
-    {
+    if (m_driverController.getAButtonPressed()) {
       shooterConfig++;
-      if(shooterConfig==3)
-        shooterConfig=0;
+      if (shooterConfig == 3)
+        shooterConfig = 0;
       m_Shooter.shooterConfig(shooterConfig, m_ShooterMotor, m_hoodMotor);
     }
 
-    if(m_driverController.getBackButtonPressed())
-    {
-      if(m_Shooter.autoalign(m_turretMotor))
-      {
+    if (m_driverController.getBackButtonPressed()) {
+      if (m_Shooter.autoalign(m_turretMotor)) {
         m_Shooter.fire(m_transferMotor);
       }
     }
 
-    if(m_driverController.getBButtonReleased())
-    {
+    if (m_driverController.getBackButtonReleased()) {
       m_Shooter.stopFire(m_transferMotor);
     }
 
-    if(m_driverController.getRawButtonPressed(8))
-    {
+    if (m_driverController.getPOV() == 270) {
+      System.out.println("----------------Xbox Controller: D Pad Left----------------");
       m_Climb.leftBarRunner(m_funnel);
     }
 
-    if(m_driverController.getRawButtonPressed(10))
-    {
+    if (m_driverController.getPOV() == 90) {
+      System.out.println("----------------Xbox Controller: D Pad Right----------------");
       m_Climb.rightBarRunner(m_funnel);
     }
 
-    if(m_driverController.getRawButtonReleased(8))
-    {
-      m_Climb.stopBarRunner(m_funnel);
+    if (m_driverController.getBumperPressed(Hand.kLeft)) {
+      try {
+        m_Climb.climb(m_ClimbMotor, s_Latch);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
 
-    if(m_driverController.getRawButtonReleased(10))
-    {
-      m_Climb.stopBarRunner(m_funnel);
+    if (m_driverController.getBumperPressed(Hand.kRight)) {
+      try {
+        m_Climb.retract(m_ClimbMotor, s_Latch);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
 
-    if(m_driverController.getBumperPressed(Hand.kLeft))
-    {
-      try 
-      {
-		    m_Climb.climb(m_ClimbMotor, s_Latch);
-      } 
-      catch (InterruptedException e) 
-      {
-	    	e.printStackTrace();
-	    }
-    }
-
-    if(m_driverController.getBumperPressed(Hand.kRight))
-    {
-      try 
-      {
-		    m_Climb.retract(m_ClimbMotor,s_Latch);
-      } 
-      catch (InterruptedException e) 
-      {
-	    	e.printStackTrace();
-	    }
-    }
-
-    if(m_driverController.getBumperReleased(Hand.kLeft))
-    {
+    if (m_driverController.getBumperReleased(Hand.kLeft)) {
       m_Climb.stopClimb(m_ClimbMotor);
     }
 
-    if(m_driverController.getBumperReleased(Hand.kRight))
-    {
+    if (m_driverController.getBumperReleased(Hand.kRight)) {
       m_Climb.stopClimb(m_ClimbMotor);
     }
 
-    if(m_driverController.getBButtonPressed())
-    {
+    if (m_driverController.getBButtonPressed()) {
+      System.out.println("----------------Xbox Controller: B Button Pressed----------------");
+      try {
+        if (isLatched) {
+          m_Climb.unlatch(m_ClimbMotor, s_Latch);
+        } else if (!isLatched) {
+          m_Climb.latch(m_ClimbMotor, s_Latch);
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (m_driverController.getPOV() == 180) {
+      try {
+        m_Climb.controlPanelRotation(m_funnel);
+        System.out.println("----------------Xbox Controller: D Pad Down----------------");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (m_driverController.getYButtonPressed()) {
       try 
       {
-        if(isLatched)
-        {
-          m_Climb.unlatch(m_ClimbMotor,s_Latch);
-        }
-        else if(!isLatched)
-        {
-          m_Climb.latch(m_ClimbMotor,s_Latch);
-        }
+        m_Climb.latch(m_ClimbMotor, s_Latch);
+        System.out.println("----------------Xbox Controller: Y Button Detected----------------");
       } 
       catch (InterruptedException e) 
       {
-	    	e.printStackTrace();
-	    }
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
 
-    if(m_driverController.getYButtonPressed())
-    {
-      try 
-      {
-		    m_Climb.controlPanelRotation(m_funnel);
-      } 
-      catch (InterruptedException e) 
-      {
-		    e.printStackTrace();
-	    }
-    }
-
-    if(m_driverController.getRawButtonPressed(13) && m_driverController.getAButtonPressed())
+    if(m_driverController.getPOV() == 0 && m_driverController.getAButtonPressed())
     {
       m_Climb.controlPanelPosition("green", m_funnel, m_colorSensor);
       isPositioning = true;
+      System.out.println("----------------Xbox Controller: D Pad Up and 'A' Button Detected----------------");
     }
 
-    if(m_driverController.getRawButtonPressed(13) && m_driverController.getBButtonPressed())
+    if(m_driverController.getPOV() == 0 && m_driverController.getBButtonPressed())
     {
       m_Climb.controlPanelPosition("red", m_funnel, m_colorSensor);
       isPositioning = true;
+      System.out.println("----------------Xbox Controller: D Pad Up and 'B' Button Detected----------------");
     }
 
-    if(m_driverController.getRawButtonPressed(13) && m_driverController.getYButtonPressed())
+    if(m_driverController.getPOV() == 0 && m_driverController.getYButtonPressed())
     {
       m_Climb.controlPanelPosition("yellow", m_funnel, m_colorSensor);
       isPositioning = true;
+      System.out.println("----------------Xbox Controller: D Pad Up and 'Y' Button Detected----------------");
     }
 
-    if(m_driverController.getRawButtonPressed(13) && m_driverController.getXButtonPressed())
+    if(m_driverController.getPOV() == 0 && m_driverController.getXButtonPressed())
     {
       m_Climb.controlPanelPosition("blue",m_funnel, m_colorSensor);
       isPositioning = true;
+      System.out.println("----------------Xbox Controller: D Pad Up and 'X' Button Detected----------------");
     }
   }
 
